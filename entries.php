57a4d3a2f269da8829888964e4ff8f132322caa8
npurcella <npurcella@users.noreply.github.com>
2015-12-28 17:45:29 -0700

    Added link to single blog page.

--- entries.php
+++ entries.php
@@ -4,12 +4,12 @@
 	foreach ($results as $row) {
 		$created = date('M d Y', strtotime($row['created']));
 		if($row['picture'] != null) {
-			echo '<div id="title-and-created"><a href=#">'.$row['title'].'</a>&nbsp;&nbsp; ( '.$created.' ) </div>'.
+			echo '<div id="title-and-created"><a href="entry.php?id='.$row['id'].'">'.$row['title'].'</a>&nbsp;&nbsp; ( '.$created.' ) </div>'.
 			'<div id="content">'.$row['content'].'</div>'.
 			'<div id="picture">'.'<img src="admin/showimage.php?id='.$row['id'].'" width="80" height="60"/>'.'</div>';
 		}
 		else {
-			echo '<div id="title-and-created"><a href="#">'.$row['title'].'</a> ( '.$created.' ) </div>'.
+			echo '<div id="title-and-created"><a href="entry.php?id='.$row['id'].'">'.$row['title'].'</a> ( '.$created.' ) </div>'.
 			'<div id="content">'.$row['content'].'</div>'.
 			'<div id="picture">'.'<img src="assets/library/noImage.png" width="80" height="60"/>'.'</div>';
 		}
